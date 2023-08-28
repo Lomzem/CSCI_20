c94f7bf417cf9966ead9b6b5f58245778ae8d904
Lawjay Lee <lawjaylee1@icloud.com>
2023-08-28 16:29:14 -0700

    batman

---
+++ hello_world/hello_world.go
@@ -0,0 +1,16 @@
+/***************************************
+Hello World
+Author: Lawjay Lee
+Date Completed: 2023-08-24
+Description: Prints hello world and name to stdout
+***************************************/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World!")
+    fmt.Println("My name is Lawjay Lee")
+}
+
