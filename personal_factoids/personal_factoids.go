cb5542bae8cf641006435457cfcb0d0811c8e519
Lawjay <lawjaylee1@icloud.com>
2023-08-28 16:31:45 -0700

    more

---
+++ personal_factoids/personal_factoids.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Name: %s\n", "Lawjay Lee")
+    fmt.Printf("Favorite Fictional Character: %s\n", "Kaneki Ken")
+    fmt.Printf("Least Favorite Food: %s\n", "Gizzard")
+    fmt.Printf("Star Trek or Star Wars: %s\n\n", "Star Trek")
+
+    var favorite_quote string = "I am become death, the destroyer of worlds."
+
+    fmt.Printf("Favorite Quote:\n%s", favorite_quote)
+}
+
