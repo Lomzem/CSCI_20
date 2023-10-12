a881d294b5624167841560e3158725a68ba5d0f4
Lawjay Lee <lawjaylee1@icloud.com>
2023-10-11 22:11:17 -0700

    finished command line cuboid without lecture might check again after

--- command_line_coboid/CommandLineCuboid.go
+++ command_line_coboid/CommandLineCuboid.go
@@ -14,29 +14,29 @@ import (
 
 // Implement Cuboid data type definition here
 type Cuboid struct {
-    Width int
-    Length int
-    Height int
+	Width  int
+	Length int
+	Height int
 }
 
 // Implement pseudo-constructor function (NewCuboid) here
 func NewCuboid(width int, length int, height int) Cuboid {
-    var c Cuboid
-    c.Width = width
-    c.Length = length
-    c.Height = height
-    return c
+	var c Cuboid
+	c.Width = width
+	c.Length = length
+	c.Height = height
+	return c
 }
 
 // Implement Cuboid member function definitions here
 func (c *Cuboid) Volume() int {
-    return c.Width * c.Length * c.Height
+	return c.Width * c.Length * c.Height
 }
 
-func (c * Cuboid) Display() {
-    fmt.Println("Width:", c.Width)
-    fmt.Println("Length:", c.Length)
-    fmt.Println("Height:", c.Height)
+func (c *Cuboid) Display() {
+	fmt.Println("Width:", c.Width)
+	fmt.Println("Length:", c.Length)
+	fmt.Println("Height:", c.Height)
 }
 
 func main() {
@@ -44,10 +44,10 @@ func main() {
 
 	// Implement logic to map and assign command line argument values
 	// to inputWidth, inputLength, and inputHeight variables here
-    flag.IntVar(&inputWidth, "Width", 0, "Width of cuboid")
-    flag.IntVar(&inputLength, "Length", 0, "Length of cuboid")
-    flag.IntVar(&inputHeight, "Height", 0, "Height of cuboid")
-    flag.Parse()
+	flag.IntVar(&inputWidth, "Width", 0, "Width of cuboid")
+	flag.IntVar(&inputLength, "Length", 0, "Length of cuboid")
+	flag.IntVar(&inputHeight, "Height", 0, "Height of cuboid")
+	flag.Parse()
 
 	var customCuboid Cuboid = NewCuboid(inputWidth, inputLength, inputHeight)
 	customCuboid.Display()
