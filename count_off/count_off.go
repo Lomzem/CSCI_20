675c0032aebbd8e70e7b1ea0dba901e53e983f93
Lawjay Lee <lawjaylee1@icloud.com>
2023-10-13 22:09:22 -0700

    replaced recursive answer with while loop

--- count_off/count_off.go
+++ count_off/count_off.go
@@ -11,22 +11,28 @@ package main
 import "fmt"
 
 func main() {
-	var userNum int
-	fmt.Println("Enter a number between 1 and 10")
-	fmt.Scanln(&userNum)
-	fmt.Println()
+	var repeat string = "Y"
 
-	for i := userNum; i <= 10; i++ {
-		fmt.Println(i)
+    /*
+	I originally did a recursive answer
+    by calling main() again if the user
+    entered "Y", but a while loop is
+    probably the intended way to do this
+    */
+	for repeat == "Y" {
+		var userNum int
+		fmt.Println("Enter a number between 1 and 10")
+		fmt.Scanln(&userNum)
+		fmt.Println()
+
+		for i := userNum; i <= 10; i++ {
+			fmt.Println(i)
+		}
+
+		var repeat string
+		fmt.Println("Would you like to run the program again? (Y for yes, N for no)")
+		fmt.Scanln(&repeat)
 	}
 
-	var repeat string
-	fmt.Println("Would you like to run the program again? (Y for yes, N for no)")
-	fmt.Scanln(&repeat)
-
-	if repeat == "Y" {
-		main()
-	} else {
-		fmt.Println("\nOK, have a nice day!")
-	}
+	fmt.Println("\nOK, have a nice day!")
 }
--- sample/sample.go
+++ sample/sample.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+
+// func factorial(n int) int {
+//     var product int = 1
+//     for i := 1; i <= n; i++ {
+//         product *= i
+//     }
+//     return product
+// }
+
 func main() {
-	fmt.Println("Hello")
+    fmt.Println("Recursive 5!:", factorial(5))
 }
