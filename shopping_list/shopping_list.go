50b91d511950db47c51fefb30705733ee64d9a1c
Lawjay <lawjaylee1@icloud.com>
2023-11-09 21:40:37 -0800

    did my own algo for printing commas :P

--- shopping_list/shopping_list.go
+++ shopping_list/shopping_list.go
@@ -10,19 +10,35 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	// for standard library solution:
+	// "strings"
 )
 
 func main() {
 	var shoppingList []string
 
 	fmt.Println("Enter your shopping list. Enter * to indicate you are done:")
+
 	var userInput string
 	for userInput != "*" {
 		fmt.Scanln(&userInput)
 		shoppingList = append(shoppingList, userInput)
 	}
-	shoppingList = shoppingList[:len(shoppingList)-1]
+
 	fmt.Println("Here is your list:")
-	fmt.Println(strings.Join(shoppingList, ", "))
+
+	for i := 0; i < len(shoppingList)-2; i++ {
+		// Minus two is because the asterisk is appended to the list,
+		// and we don't want to add a comma for the last item
+		fmt.Print(shoppingList[i], ", ")
+	}
+
+	// len() - 1 would be asterisk, len() - 2 is last item
+	fmt.Print(shoppingList[len(shoppingList)-2])
+
+	// Messes with Scanln if there's no newline
+	fmt.Println()
+
+	// Solution with standard library:
+	// fmt.Println(strings.Join(shoppingList, ", "))
 }
